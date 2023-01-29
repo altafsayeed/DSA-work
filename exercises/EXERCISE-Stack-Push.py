6ee7a76103f42f2a725f7e1aed780b7a778d09de
altafsayeed <altafsayeed19@yahoo.com>
2023-01-29 00:36:41 -0800

    stacks

--- exercises/EXERCISE-Stack-Push.py
+++ exercises/EXERCISE-Stack-Push.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        
 
 class Stack:
     def __init__(self, value):
@@ -17,14 +18,36 @@ class Stack:
 
     def push(self, value):
         new_node = Node(value)
-        if self.height == 0:                # If stack is empty, set top to new node
+        if self.height == 0:
             self.top = new_node
         else:
-            new_node.next = self.top        # Set new node's next pointer to the current top
-            self.top = new_node             # Set top to the new node
+            new_node.next = self.top
+            self.top = new_node
         self.height += 1
+ 
+
+
 
 my_stack = Stack(2)
+
+print('Stack before push(1):')
+my_stack.print_stack()
+
 my_stack.push(1)
 
+print('\nStack after push(1):')
 my_stack.print_stack()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Stack before push(1):
+    2
+
+    Stack after push(1):
+    1
+    2   
+
+"""
