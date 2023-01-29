c45dd45479bccfc7cf74e7eba59912950c2313c0
altafsayeed <altafsayeed19@yahoo.com>
2023-01-29 01:10:07 -0800

    queues

--- exercises/EXERCISE-Queue-Enqueue.py
+++ exercises/EXERCISE-Queue-Enqueue.py
@@ -2,6 +2,7 @@ class Node:
     def __init__(self, value):
         self.value = value
         self.next = None
+        
 
 class Queue:
     def __init__(self, value):
@@ -15,18 +16,40 @@ class Queue:
         while temp is not None:
             print(temp.value)
             temp = temp.next
-
+        
     def enqueue(self, value):
         new_node = Node(value)
-        if self.first is None:            # If empty queue, set first and last equal to new node
+        if self.first is None:
             self.first = new_node
             self.last = new_node
         else:
-            self.last.next = new_node   # Set last node's next node to the new node
-            self.last = new_node        # Set last node equal to the new node
+            self.last.next = new_node
+            self.last = new_node
         self.length += 1
+        
+
+
 
 my_queue = Queue(1)
+
+print('Queue before enqueue(2):')
+my_queue.print_queue()
+
 my_queue.enqueue(2)
 
+print('\nQueue after enqueue(2):')
 my_queue.print_queue()
+
+
+
+"""
+    EXPECTED OUTPUT:
+    ----------------
+    Queue before enqueue(2):
+    1
+
+    Queue after enqueue(2):
+    1
+    2
+
+"""
