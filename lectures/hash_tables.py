6334f28b3a6a195433875f0a658b9e9a5ed145b2
altafsayeed <altafsayeed19@yahoo.com>
2023-02-04 23:05:35 -0800

    hash tables

--- lectures/hash_tables.py
+++ lectures/hash_tables.py
@@ -14,6 +14,17 @@ class HashTable:
         for i, val in enumerate(self.data_map):
             print(i, ": ", val)
 
+    def set_item(self, key, value):                         
+        index = self.__hash(key)                        # Call hash function to get the index where data will be stored
+        if self.data_map[index] == None:                # If there is no existing list at the index, then create one
+            self.data_map[index] = []
+        self.data_map[index].append([key, value])       # Add the key,value pair to the index using append
+
 my_hash_table = HashTable()
+
+my_hash_table.set_item('bolts', 1400)
+my_hash_table.set_item('washers', 50)
+my_hash_table.set_item('lumber', 70)
+
 
 my_hash_table.print_table()
