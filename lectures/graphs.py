df2aad947aaf404d2056ce340df093d63acbd7e7
altafsayeed <altafsayeed19@yahoo.com>
2023-02-05 22:52:04 -0800

    graphs

--- lectures/graphs.py
+++ lectures/graphs.py
@@ -12,8 +12,18 @@ class Graph:
             return True                                 
         return False
     
+    def add_edge(self, v1, v2):                         # Add an edge between 2 vertices
+        if v1 in self.adj_list.keys() and v2 in self.adj_list.keys():   # v1 and v2 must BOTH exist in the adj list keys
+            self.adj_list[v1].append(v2)                                # At v1 in the list, append v2 to add connection
+            self.adj_list[v2].append(v1)                                # At v2 in the list, append v1 to add connection
+            return True
+        return False
+    
 my_graph = Graph()
 
-my_graph.add_vertex('A')
+my_graph.add_vertex(1)
+my_graph.add_vertex(2)
+
+my_graph.add_edge(1,2)
 
 my_graph.print_graph()
