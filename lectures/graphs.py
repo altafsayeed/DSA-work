0f0a7038d66e932e50a4b8dab10a6294641e15be
altafsayeed <altafsayeed19@yahoo.com>
2023-02-20 22:11:44 -0800

    graphs

--- lectures/graphs.py
+++ lectures/graphs.py
@@ -28,6 +28,14 @@ class Graph:
                 pass
             return True
         return False
+    
+    def remove_vertex(self, vertex):
+        if vertex in self.adj_list.keys():          
+            for other_vertex in self.adj_list[vertex]:          # Go through list of edges associated with vertex
+                self.adj_list[other_vertex].remove(vertex)      # Go to other_vertices and remove edges with vertex we're trying to remove
+            del self.adj_list[vertex]                           # Once edges with other vertices are removed, we can delete the vertex
+            return True
+        return False
 
     
 my_graph = Graph()
@@ -35,11 +43,14 @@ my_graph = Graph()
 my_graph.add_vertex('A')
 my_graph.add_vertex('B')
 my_graph.add_vertex('C')
+my_graph.add_vertex('D')
 
 my_graph.add_edge('A','B')
-my_graph.add_edge('B','C')
-my_graph.add_edge('C','A')
+my_graph.add_edge('A','C')
+my_graph.add_edge('A','D')
+my_graph.add_edge('B','D')
+my_graph.add_edge('C','D')
 
-my_graph.remove_edge('A','B')
+my_graph.remove_vertex('D')
 
 my_graph.print_graph()
