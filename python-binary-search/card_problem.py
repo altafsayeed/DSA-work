f684bbeeb4d90e783bde430186b22a801f2409ab
altafsayeed <altafsayeed19@yahoo.com>
2023-03-17 00:14:36 -0700

    card problem

--- python-binary-search/card_problem.py
+++ python-binary-search/card_problem.py
@@ -1,7 +1,7 @@
 def locate_card(cards, query):
     position = 0            # Create position variable with value 0
 
-    while position < len(cards):             # Set up a loop for repetition
+    while position < len(cards):            # Loop as long as current position value is less than the length of the cards. If it becomes equal to the length, we have reached the end
         if cards[position] == query:        # Check if element at current position matches the query
             return position                 # Answer found, return and exit
         position += 1                       # If it doesn't match, increment the position
